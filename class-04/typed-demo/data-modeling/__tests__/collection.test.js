22fc3cf16604fb7d01d37b66bfde97d5398f9289
JacobKnaack <jacobknck@gmail.com>
2022-01-13 12:54:44 -0800

    adds test to validate colletion interface

---
+++ class-04/typed-demo/data-modeling/__tests__/collection.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { db, MessageCollection, AuthorCollection } = require('../models');
+
+beforeAll(async () => {
+  await db.sync();
+});
+afterAll(async () => {
+  await db.drop();
+});
+
+describe('Testing the Collection module data storage', () => {
+  let authorId;
+  let messageId;
+
+  it ('should create an author', async () => {
+    let newAuthor = await AuthorCollection.create({name: 'Jacob'});
+    authorId = newAuthor.id;
+    expect(newAuthor.name).toEqual('Jacob');
+  });
+
+  it('should create a message', async () => {
+    let newMessage = await MessageCollection.create({text: 'test', authorId });
+    messageId = newMessage.id;
+    expect(newMessage.text).toEqual('test');
+  });
+
+  it ('should read from messages', async () => {
+    let messages = await MessageCollection.read();
+    expect(messages.length).toEqual(1);
+  });
+});
