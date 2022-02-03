59c4248be50479e9cb494dcd3ffd9c0ed343a3e1
JacobKnaack <jacobknck@gmail.com>
2022-02-03 12:44:32 -0800

    adds solution to warm up prompt

---
+++ class-19/warm-up/removeNode.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function removeNode(node) {
+
+  if (!node || !node.value) {
+    throw new Error('Please pass a valid node')
+  }
+
+  let temp = node.next;
+  node.value = temp.value;
+  node.next = temp.next;
+  temp.next = null;
+
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+let list = new LinkedList();
+let nodeToRemove = new Node(14);
+
+list.head = new Node(20);
+list.head.next = nodeToRemove;
+list.head.next.next = new Node(30);
+
+console.log(JSON.stringify(list));
+removeNode(nodeToRemove);
+console.log(JSON.stringify(list)); 
